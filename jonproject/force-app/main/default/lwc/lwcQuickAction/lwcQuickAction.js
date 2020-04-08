714eb99e6e1e67f884a41f65e1d35cc36964b050
Jonathan Almqvist <jonathan.almqvist@gmail.com>
2020-04-08 10:45:14 +0200

    Backup

--- jonproject/force-app/main/default/lwc/lwcQuickAction/lwcQuickAction.js
+++ jonproject/force-app/main/default/lwc/lwcQuickAction/lwcQuickAction.js
@@ -1,9 +1,13 @@
 import { LightningElement, api} from 'lwc';
 import getContactList from '@salesforce/apex/ContactController.getContactList';
+import { publish, createMessageContext,releaseMessageContext, subscribe, unsubscribe } from 'lightning/messageService';
+import messageChannelShowcase from '@salesforce/messageChannel/messageChannelShowcase__c';
+
 export default class LwcQuickAction extends LightningElement {
     @api recordId;
     contacts;
 
+    context = createMessageContext();
     //First method run
     connectedCallback() {
         this.getContactList();
@@ -18,6 +22,7 @@ export default class LwcQuickAction extends LightningElement {
         const message = {
             loading: false
         };
+        publish(this.context, messageChannelShowcase, message);
     }
     getContactList() {
         getContactList()
